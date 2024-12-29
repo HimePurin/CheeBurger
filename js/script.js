105333197945f97a78b118447623e6127e9785c3
HimePurin <purinchii03@gmail.com>
2024-12-30 05:59:06 +0700

    methetcuu

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Đảm bảo carousel tự động chuyển đổi liên tục
+$('#carouselExampleIndicators1').carousel({
+    interval: 3000, // Thời gian giữa các slide (3 giây)
+    pause: false // Không dừng khi hover
+});
